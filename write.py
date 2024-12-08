792abdcbb9683744acc28180de189c06ff7fd972
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2024-12-08 20:56:10 +0000

    Change naming

--- write.py
+++ write.py
@@ -1,20 +1,20 @@
 from typing import Mapping
 import users_pb2
 
-users = (
+user_data = (
     {'id': 1, 'name': 'John Doe', 'email': 'john@apian.aero'},
     {'id': 2, 'name': 'Jane Doe', 'email': 'jane@apian.aero'},
 )
-users_proto = users_pb2.Users()
+users = users_pb2.Users()
 
 def add_user(user: Mapping[str, str | int]):
-    u = users_proto.user.add()
+    u = users.user.add()
     u.id = user['id']
     u.full_name = user['name']
     u.email = user['email']
 
-for user in users:
+for user in user_data:
     add_user(user)
 
 with open('msg.pb', 'wb') as f:
-    f.write(users_proto.SerializeToString())
+    f.write(users.SerializeToString())
